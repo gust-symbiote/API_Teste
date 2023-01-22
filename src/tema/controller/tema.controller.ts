52fff1cde3cfeb948b76014017340909f566905d
Symbiote Dev <123264193+gust-symbiote@users.noreply.github.com>
2023-01-22 14:10:43 -0300

    Add files via upload

--- src/tema/controller/tema.controller.ts
+++ src/tema/controller/tema.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards, Delete } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseGuards } from "@nestjs/common";
+import { HttpStatus } from "@nestjs/common/enums";
+import { ParseIntPipe } from "@nestjs/common/pipes";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger/dist";
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 import { Tema } from "../entities/tema.entity";
 import { TemaService } from "../service/tema.service";
@@ -8,45 +10,47 @@ import { TemaService } from "../service/tema.service";
 @UseGuards(JwtAuthGuard)
 @Controller('/tema')
 @ApiBearerAuth()
-export class TemaCrontoller{
-    constructor(private readonly temaService: TemaService){}
+export class TemaController {
+    constructor(private readonly temaService: TemaService) { }
 
     @Get()
-        @HttpCode(HttpStatus.OK)
-             findAll(): Promise<Tema[]> {
-                return this.temaService.findAll()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Tema[]> {
+        return this.temaService.findAll();
     }
-
+  
     @Get('/:id')
-        @HttpCode(HttpStatus.OK)
-            findById(@Param('id', ParseIntPipe) id: number): Promise<Tema> {
-                return this.temaService.findById(id)
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Tema> {
+        return this.temaService.findById(id)
     }
 
-    @Get('/assunto/:assunto')
-        @HttpCode(HttpStatus.OK)
-            findByAssunto(@Param('assunto') assunto:string): Promise<Tema[]>{
-                return this.temaService.findByAssunto(assunto)
+    @Get('/descricao/:descricao')
+    @HttpCode(HttpStatus.OK)
+    findByDescricao(@Param('descricao') descricao: string): Promise<Tema[]> {
+        return this.temaService.findByDescricao(descricao)
     }
+
 
     @Post()
-        @HttpCode(HttpStatus.CREATED)
-            create(@Body() tema: Tema): Promise<Tema>{
-                return this.temaService.create(tema)
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() tema: Tema): Promise<Tema> {
+        return this.temaService.create(tema)
     }
 
     @Put()
-        @HttpCode(HttpStatus.OK)
-            update(@Body() tema: Tema): Promise<Tema>{
-                return this.temaService.update(tema)
+    @HttpCode(HttpStatus.OK)
+    update(@Body() tema: Tema): Promise<Tema> {
+        return this.temaService.update(tema)
     }
-    
-    
+
+
     @Delete('/:id')
-        @HttpCode(HttpStatus.NO_CONTENT)
-            delete(@Param('id', ParseIntPipe) id: number){
-                return this.temaService.delete(id)
+    @HttpCode(HttpStatus.NO_CONTENT)
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return this.temaService.delete(id)
     }
+
+
 
 }
-
